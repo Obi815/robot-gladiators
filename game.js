8aa4481d532b8cdd0eec049b762b6520b44d7215
Obi815 <80441506+Obi815@users.noreply.github.com>
2022-11-07 14:24:22 -0800

    first save

---
+++ game.js
@@ -0,0 +1,8 @@
+// This creates a functions called "fight"
+function fight() {
+    window.alert("the fight has begun");
+}
+//fight();
+
+var playerName = window.prompt("What is your robot's name?");
+console.log(playerName);
