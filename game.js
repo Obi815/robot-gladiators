6dbef0e6466a22565d32293867c4bdb8db104822
Obi815 <80441506+Obi815@users.noreply.github.com>
2022-11-07 15:33:28 -0800

    learned console logs

--- game.js
+++ game.js
@@ -6,3 +6,12 @@ function fight() {
 
 var playerName = window.prompt("What is your robot's name?");
 console.log(playerName);
+
+console.log("this logs a string, good for leaving yoursel;f a message");
+// this will do math and log 20
+console.log(10 +10);
+//what is this 
+console.log("Our robots's naem is " + playerName);
+
+var name = "your name";
+console.log(name); 
